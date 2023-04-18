64ec326c36df2eaae02e263eaa4f278f22499e7e
dj-park <sologoal@naver.com>
2023-04-18 19:40:12 -0400

    1)adjust cifar10 test data sizes

--- input_src/finn_cnn_cifar10/host/test_data.h
+++ input_src/finn_cnn_cifar10/host/test_data.h
@@ -13,6 +13,19 @@ const uint8_t expected_labels[9600] = {
 };
 
 const uint8_t test_data[9600 * 32 * 32 * 3] = {
-    #include "test_set_0.dat",
-    #include "test_set_1.dat"    
+    #include "test_data_0.dat"
+    #include "test_data_1.dat"
+    #include "test_data_2.dat"
+    #include "test_data_3.dat"
+    #include "test_data_4.dat"
+    #include "test_data_5.dat"
+    #include "test_data_6.dat"
+    #include "test_data_7.dat"
+    #include "test_data_8.dat"
+    #include "test_data_9.dat"
+    #include "test_data_10.dat"
+    #include "test_data_11.dat"
+    #include "test_data_12.dat"
+    #include "test_data_13.dat"
+    #include "test_data_14.dat"
 };
